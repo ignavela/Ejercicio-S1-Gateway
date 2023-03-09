c16ecc4bcc2f7ffe935cb482c923aef7911fe403
Ignacio Vela <68652925+ignavela@users.noreply.github.com>
2023-03-09 20:45:58 -0300

    se mejora el control de la moneda
    ahora tambien acepta minusculas

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import json
 # Se ingresa el monto por linea de comando
 monto = input('Ingresa el monto a convertir: ')
 
+# Si el valor tiene decimales, debe ser con '.'
 if "," in monto:
     print("Error: no utilizar ',' en el monto para especificar decimales. Utilizar '.'")
     exit()
@@ -13,11 +14,15 @@ else:
 # Se elige la moneda por linea de comando
 moneda = input('Selecciona la moneda [ARS o EUR]: ')
 
-if moneda not in("ARS", "EUR"):
-    print("La moneda seleccionada no es correcta, solo acepta ARS o EUR por el momento.")
-    exit()
-else:
-    pass
+# Controla que la moneda ingresada sea solo ARS o EUR. Ahora tambien acepta minusculas.
+match moneda:
+    case "ARS"|"EUR":
+        pass
+    case "ars"|"eur":
+        moneda = moneda.upper()
+    case other:
+        print("La moneda seleccionada no es correcta, solo acepta ARS o EUR por el momento.")
+        exit()
 
 # Se guarda la API Key, los parametros y la URL de Currency API para obtener las cotizaciones
 api_key = "33c0924ef4c5256e1341fc05eb4e96d701f7ea03"
